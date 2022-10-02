827a24be41846d9c60c37ac0a9e03c39b3cd8465
16YURY <llomm@tut.by>
2022-10-02 15:31:26 +0300

    1

---
+++ src/main/java/lesson3/Main.java
@@ -0,0 +1,25 @@
+package lesson3;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        User user = User.builder()
+//                .setName("Ivan")
+//                .setSurname("Ivanov")
+//                .setAddress("Address")
+//                .setEmail("123@ya.ru")
+//                .build();
+
+        // DSL - domain specific language
+
+        RequestSpecification requestSpecification =
+                new RequestSpecBuilder()
+                        .build();
+    }
+}
